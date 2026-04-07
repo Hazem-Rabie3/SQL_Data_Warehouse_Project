26770cd5e21facbef2c1f41206e16b4a7ca468bf
Hazem Rabie <hazem.rabie2004@gmail.com>
2026-04-07 02:58:24 +0200

    Update ddl_bronze.sql

--- scripts/ddl_bronze.sql
+++ scripts/ddl_bronze.sql
@@ -14,18 +14,20 @@ Description:
 
 */
 
-
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info(
 cst_id INT,
 cst_key nvarchar(50),
 cst_firstname nvarchar(50),
 cst_lastname nvarchar(50),
-cst_material_status nvarchar(20),
+cst_marital_status nvarchar(20),
 cst_gndr nvarchar(20),
 cst_create_date datetime
 )
 
-
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
 CREATE TABLE bronze.crm_prd_info(
 prd_id INT,
 prd_key nvarchar(50),
@@ -35,7 +37,8 @@ prd_line nvarchar(20),
 prd_start_dt datetime,
 prd_end_dt datetime
 )
-
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
 CREATE TABLE bronze.crm_sales_details(
 sls_ord_num nvarchar(50),
 sls_prd_key nvarchar(50),
@@ -48,17 +51,23 @@ sls_quantity INT,
 sls_price INT
 )
 
+IF OBJECT_ID('bronze.erp_CUST_AZ12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_CUST_AZ12;
 CREATE TABLE bronze.erp_CUST_AZ12(
 CID nvarchar(50),
 BDATE date,
 GEN nvarchar(20)
 )
 
+IF OBJECT_ID('bronze.erp_LOC_A101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_LOC_A101;
 CREATE TABLE bronze.erp_LOC_A101(
 CID nvarchar(50),
 CNTRY nvarchar(20)
 )
 
+IF OBJECT_ID('bronze.erp_PX_CAT_G1V2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_PX_CAT_G1V2;
 CREATE TABLE bronze.erp_PX_CAT_G1V2(
 ID nvarchar(50),
 CAT nvarchar(50),
